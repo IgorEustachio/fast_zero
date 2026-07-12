d87bf182764ddd8c342d9cc96ffe4816142fbcc4
IgorEustachio <igor.daniel.eustachio@gmail.com>
2026-07-11 21:20:33 -0300

    feat: adiciona primeira migração Alembic e cria tabela de usuários

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 
 from fast_zero.app import app
 from fast_zero.models import table_registry
+
+from contextlib import contextmanager
+from datetime import datetime
+
 
 
 @pytest.fixture
@@ -29,3 +33,20 @@ def session():
     # 4. Após o teste terminar, DESTRÓI tudo
     table_registry.metadata.drop_all(engine)  # apaga as tabelas
     engine.dispose()                         # fecha as conexões
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2024,1,1)):
+
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time    
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
